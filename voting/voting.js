912ea2b59584fda525ce0f44cba9f7f662e73402
Juha Tauriainen <juha@bin.fi>
2024-11-25 14:03:50 +0200

    Add vote results

--- voting/voting.js
+++ voting/voting.js
@@ -20,11 +20,31 @@ for (let i = 1; i <= voterCount; i++) {
 
   if (name) {
     for (let j = 0; j < candidates.length; j++) {
-      if(candidates[j].name === name) {
+      if(candidates[j].name.toLowerCase() === name.toLowerCase()) {
         candidates[j].votes++
       }
     }
   }
 }
+
+candidates.sort((a, b) =>
+    b.votes - a.votes
+)
+
+const winner = candidates[0]
+console.log(`The winner is ${winner.name} with ${winner.votes}`)
+console.log("Results:")
+
+for (const candidate of candidates) {
+  console.log(` - ${candidate.name}: ${candidate.votes} votes`)
+}
+
+/*
+The winner is pamela with 3 votes.
+results:
+pamela: 3 votes
+frank: 1 votes
+ellie: 1 votes
+ */
 
 console.log("candidates", candidates)
